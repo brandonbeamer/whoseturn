7e8fe46e2f39c9dd6ef54479c42e9184ba84d465
Brandon Beamer <brandon.r.beamer@gmail.com>
2020-02-24 08:13:53 +0000

    Got /register and /login working. Set up models for /dashboard.

--- models.py
+++ models.py
@@ -1,3 +1,30 @@
+import uuid
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Task(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=30)
+    members = models.ManyToManyField(User)
+
+    def __str__(self):
+        return f"Task: {self.name}"
+
+    def get_absolute_url(self):
+        return reverse('wt-logentries', kwargs={'uuid': self.id})
+
+class LogEntry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='+',
+        null=True)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    comment = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.user} did '{self.task}' at {self.timestamp}"
+
+class UserSettings(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    timezone = models.CharField(max_length=50, default='UTC')
--- views.py
+++ views.py
@@ -1,3 +1,65 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, FormView, View
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import Group
+from django.contrib.auth.mixins import UserPassesTestMixin
 
+# from django.contrib.auth.forms import AuthenticationForm
+# from django.contrib.auth.forms import UserCreationForm
+from .forms import FullUserCreationForm, CustomAuthenticationForm
+from .constants import APP_GROUP_NAME
 # Create your views here.
+
+def logged_in_test(self):
+    """
+    Test function for UserPassesTestMixin
+
+    Verifies that user is logged in and member of App Group
+    """
+    user = self.request.user
+    if not user.is_authenticated:
+        return False
+
+    if not user.groups.filter(name=APP_GROUP_NAME).exists():
+        return False
+
+    return True
+
+
+class LoginView(FormView):
+    template_name = 'whoseturn/login.html'
+    form_class = CustomAuthenticationForm
+    success_url = reverse_lazy('wt-dashboard')
+
+    def form_valid(self, form):
+        login(self.request, form.get_user())
+        return super().form_valid(form)
+
+class RegisterView(FormView):
+    template_name = 'whoseturn/register.html'
+    form_class = FullUserCreationForm
+    success_url = reverse_lazy('wt-login')
+
+    def form_valid(self, form):
+        # Might need to make groups
+        try:
+            AppGroup = Group.objects.get(name=APP_GROUP_NAME)
+        except:
+            AppGroup = Group(name=APP_GROUP_NAME)
+            AppGroup.save()
+
+        user = form.save(commit=True)
+        user.groups.set([AppGroup])
+        user.save()
+        return super().form_valid(form)
+
+class DashboardView(UserPassesTestMixin, TemplateView):
+    test_func = logged_in_test
+    template_name = 'whoseturn/dashboard.html'
+
+class LogEntriesView(UserPassesTestMixin, TemplateView):
+    def test_func(self):
+        pass
+        # TODO: IMPLEMENET
+    pass
